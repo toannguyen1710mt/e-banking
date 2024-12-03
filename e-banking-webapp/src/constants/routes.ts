48864a09fa360cc9870578a5e70101354e45193e
Hoang Van Nguyen <hoang.nguyenvan@asnet.com.vn>
2024-12-03 11:29:07 +0700

    Feature / Create sitemap and robots files

---
+++ e-banking-webapp/src/constants/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  LANDING_PAGE: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  HOME: '/home',
+  ANALYTICS: '/analytics',
+  TRANSACTIONS: '/transactions',
+  BALANCE: '/balance',
+  TRANSFER: '/transfer',
+  PROFILE: '/profile',
+};
