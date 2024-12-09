3be61640b7d36339a16e250ec7d537b42b183a90
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-12-09 16:49:54 +0700

    feat: Implement unit testing for the Navbar component

---
+++ e-banking-webapp/src/components/common/Navbar/Navbar.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { NavbarContent, Navbar as NavbarNextUI } from '@nextui-org/react';
+import '@testing-library/jest-dom';
+import { NavbarItem } from '@/mocks';
+import Navbar from '.';
+
+describe('Navbar component', () => {
+  test('renders with default type', () => {
+    const { container } = render(
+      <NavbarNextUI>
+        <NavbarContent className='gap-[59px] font-normal text-transparentBlack'>
+          <Navbar navbarItem={NavbarItem} />
+        </NavbarContent>
+      </NavbarNextUI>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
