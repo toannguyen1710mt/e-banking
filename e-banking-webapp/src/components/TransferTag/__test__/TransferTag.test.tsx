f06886354533932e41a47d811d8ec7548683de8b
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-12-13 18:06:27 +0700

    fix: delete mock code in test file of component TransferTag

--- e-banking-webapp/src/components/TransferTag/__test__/TransferTag.test.tsx
+++ e-banking-webapp/src/components/TransferTag/__test__/TransferTag.test.tsx
@@ -1,36 +1,23 @@
 // Libs
 import { render, screen } from '@testing-library/react';
 
-// Components
-import { GlobalIcon, TransferTag, UserIcon } from '@/components';
+// constants
+import { TRANSFER_TABS } from '@/constants';
 
-const mockProps = [
-  {
-    keyTab: 'account',
-    title: 'To my Account',
-    description: 'Instant transfer between your own accounts',
-    icon: <UserIcon width={32} height={32} />,
-    content: <p>Account</p>,
-  },
-  {
-    keyTab: 'global',
-    title: 'Global Transfer',
-    description: 'Transfer Money across the globe',
-    icon: <GlobalIcon width={32} height={32} />,
-    content: <p>Global</p>,
-  },
-];
+// Components
+import { TransferTag } from '@/components';
 
 describe('TransferTag Component', () => {
   test('renders correctly and matches snapshot', () => {
-    const { container } = render(<TransferTag TransferTabs={mockProps} />);
+    const { container } = render(<TransferTag TransferTabs={TRANSFER_TABS} />);
+
     expect(container).toMatchSnapshot();
   });
 
   test('renders all tabs with correct titles and descriptions', () => {
-    render(<TransferTag TransferTabs={mockProps} />);
+    render(<TransferTag TransferTabs={TRANSFER_TABS} />);
 
-    mockProps.forEach(({ title, description }) => {
+    TRANSFER_TABS.forEach(({ title, description }) => {
       expect(screen.getByText(title)).toBeInTheDocument();
       expect(screen.getByText(description)).toBeInTheDocument();
     });
