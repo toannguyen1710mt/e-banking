c945deed03fab6f8f04e1f007019b0eada1fdffe
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-12-13 15:36:08 +0700

    refactor: create common function to render modal in test file

--- e-banking-webapp/src/components/common/Modal/__test__/Modal.test.tsx
+++ e-banking-webapp/src/components/common/Modal/__test__/Modal.test.tsx
@@ -5,33 +5,32 @@ import '@testing-library/jest-dom';
 // Components
 import { Modal } from '@/components';
 
+const renderModal = (
+  isOpen: boolean,
+  children: React.ReactNode = <div>Modal Content</div>,
+  onClose = jest.fn(),
+) => {
+  return render(
+    <Modal isOpen={isOpen} onClose={onClose}>
+      {children}
+    </Modal>,
+  );
+};
+
 describe('Modal component', () => {
   it('should match snapshot for Modal', () => {
-    const { container } = render(
-      <Modal isOpen={true} onClose={jest.fn()}>
-        <div>Modal Content</div>
-      </Modal>,
-    );
-
+    const { container } = renderModal(true);
     expect(container).toMatchSnapshot();
   });
 
   it('should render the modal content when isOpen is true', () => {
-    render(
-      <Modal isOpen={true} onClose={jest.fn()}>
-        <div>Modal Content</div>
-      </Modal>,
-    );
+    renderModal(true);
 
     expect(screen.getByText('Modal Content')).toBeInTheDocument();
   });
 
   it('should not render the modal content when isOpen is false', () => {
-    render(
-      <Modal isOpen={false} onClose={jest.fn()}>
-        <div>Modal Content</div>
-      </Modal>,
-    );
+    renderModal(false);
 
     expect(screen.queryByText('Modal Content')).not.toBeInTheDocument();
   });
