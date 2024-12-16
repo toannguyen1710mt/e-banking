a355bfb4e881079e855526fc0d3e1bc52d0f8d94
Ngan.Do <ngan.do@asnet.com.vn>
2024-12-16 17:01:13 +0700

    fix: invalid DOM property

--- e-banking-webapp/src/components/icons/SendIcon.tsx
+++ e-banking-webapp/src/components/icons/SendIcon.tsx
@@ -6,7 +6,7 @@ export const SendIcon = () => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <g clip-path='url(#clip0_1057_534)'>
+    <g clipPath='url(#clip0_1057_534)'>
       <path
         d='M18.3337 1.66666L9.16699 10.8333'
         stroke='currentColor'
