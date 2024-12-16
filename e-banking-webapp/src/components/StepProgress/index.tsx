5e483e3a68f080f4c047e173d97b07db01759a5c
Ngan.Do <ngan.do@asnet.com.vn>
2024-12-16 11:50:36 +0700

    fix: apply cn to collect styles

--- e-banking-webapp/src/components/StepProgress/index.tsx
+++ e-banking-webapp/src/components/StepProgress/index.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@nextui-org/react';
+
 interface StepProgressProps {
   steps: number;
   activeStep: number;
@@ -6,15 +8,18 @@ interface StepProgressProps {
 export const StepProgress: React.FC<StepProgressProps> = ({
   steps,
   activeStep,
-}) => (
-  <div className='absolute bottom-0 flex w-full items-center justify-between gap-2'>
-    {Array.from({ length: steps }).map((_, index) => (
-      <div
-        key={index}
-        className={`h-2 w-1/4 rounded-full bg-secondary-300 transition-all ${
-          index === activeStep ? '' : 'opacity-10'
-        }`}
-      />
-    ))}
-  </div>
-);
+}) => {
+  return (
+    <div className='absolute bottom-0 flex w-full items-center justify-between gap-2'>
+      {Array.from({ length: steps }).map((_, index) => (
+        <div
+          key={index}
+          className={cn(
+            'h-2 w-1/4 rounded-full bg-secondary-300 transition-all',
+            { 'opacity-10': index !== activeStep },
+          )}
+        />
+      ))}
+    </div>
+  );
+};
