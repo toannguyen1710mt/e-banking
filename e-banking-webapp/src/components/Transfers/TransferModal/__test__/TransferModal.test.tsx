11d940b81e51642a5bccea335cee9263b44357e4
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-12-17 14:34:41 +0700

    chore: update mock for use session

--- e-banking-webapp/src/components/Transfers/TransferModal/__test__/TransferModal.test.tsx
+++ e-banking-webapp/src/components/Transfers/TransferModal/__test__/TransferModal.test.tsx
@@ -1,13 +1,27 @@
 // Libs
 import { render } from '@testing-library/react';
-
-// Components
 import { TransferModal } from '@/components/Transfers/TransferModal';
+import { useSession } from 'next-auth/react';
+
+jest.mock('next-auth/react', () => ({
+  __esModule: true,
+  default: jest.fn().mockReturnValue({}),
+  useSession: jest.fn(),
+}));
 
 describe('TransferModal component', () => {
+  const mockUseSession = useSession as jest.Mock;
   it('should match snapshot for TransferModal', () => {
+    mockUseSession.mockReturnValue({
+      data: {
+        user: { name: 'admin' },
+        expires: '2024-01-01T00:00:00.000Z',
+      },
+      status: 'authenticated',
+    });
+
     const { container } = render(
-      <TransferModal isOpen={true} onClose={() => jest.fn()} />,
+      <TransferModal isOpen={true} onClose={() => {}} />,
     );
 
     expect(container).toMatchSnapshot();
