7ced568805f258916d24f27105d40d72332f179a
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-12-18 17:32:46 +0700

    feat: implement show ui for balance & transfer modal

--- e-banking-webapp/src/components/QuickAction/__test__/QuickAction.test.tsx
+++ e-banking-webapp/src/components/QuickAction/__test__/QuickAction.test.tsx
@@ -1,23 +1,50 @@
 // Libs
 import { render, screen } from '@testing-library/react';
 
-// Constants
-import { QUICK_ACTIONS } from '@/constants';
-
 // Components
-import { QuickAction } from '@/components';
+import {
+  FocusIcon,
+  QuickAction,
+  RefreshIcon,
+  SendIcon,
+  TransactionIcon,
+} from '@/components';
 
+const mockQuickActions = [
+  {
+    icon: FocusIcon,
+    label: 'Top Up',
+    isDisabled: true,
+    onClick: jest.fn(),
+  },
+  {
+    icon: SendIcon,
+    label: 'Transfer',
+    onClick: jest.fn(),
+  },
+  {
+    icon: RefreshIcon,
+    label: 'Request',
+    isDisabled: true,
+    onClick: jest.fn(),
+  },
+  {
+    icon: TransactionIcon,
+    label: 'Balance',
+    onClick: jest.fn(),
+  },
+];
 describe('QuickAction component', () => {
   test('should match snapshot for QuickAction', () => {
-    const { container } = render(<QuickAction actions={QUICK_ACTIONS} />);
+    const { container } = render(<QuickAction />);
 
     expect(container).toMatchSnapshot();
   });
 
   test('should disable buttons when `isDisabled` is true', () => {
-    render(<QuickAction actions={QUICK_ACTIONS} />);
+    render(<QuickAction />);
 
-    QUICK_ACTIONS.forEach((action) => {
+    mockQuickActions.forEach((action) => {
       const button = screen.getByText(action.label).closest('button');
 
       if (action.isDisabled) {
--- e-banking-webapp/src/components/QuickAction/index.tsx
+++ e-banking-webapp/src/components/QuickAction/index.tsx
@@ -1,9 +1,19 @@
 'use client';
 
-import { Card } from '@nextui-org/react';
+// Libs
+import { Card, useDisclosure } from '@nextui-org/react';
 
 // Components
-import { Button, Text } from '@/components';
+import {
+  BalanceModal,
+  Button,
+  FocusIcon,
+  RefreshIcon,
+  SendIcon,
+  Text,
+  TransactionIcon,
+  TransferModal,
+} from '@/components';
 
 interface Action {
   icon: () => JSX.Element;
@@ -12,36 +22,84 @@ interface Action {
   onClick: () => void;
 }
 
-interface QuickActionsProps {
-  actions: Action[];
-}
+export const QuickAction = () => {
+  const {
+    isOpen: isOpenTransferModal,
+    onOpen: onOpenTransferModal,
+    onClose: onCloseTransferModal,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenBalanceModal,
+    onOpen: onOpenBalanceModal,
+    onClose: onCloseBalanceModal,
+  } = useDisclosure();
 
-export const QuickAction = ({ actions }: QuickActionsProps) => (
-  <Card className='flex flex-col gap-2 rounded-md bg-background-500 px-[6px] py-2'>
-    <Text as='h3' className='text-xs font-semibold'>
-      Quick Action
-    </Text>
-    <ul className='flex justify-between rounded-md bg-softWhite'>
-      {actions.map((action) => {
-        const { icon, label, isDisabled, onClick } = action;
+  const QUICK_ACTIONS: Action[] = [
+    {
+      icon: FocusIcon,
+      label: 'Top Up',
+      isDisabled: true,
+      onClick: () => {},
+    },
+    {
+      icon: SendIcon,
+      label: 'Transfer',
+      onClick: onOpenTransferModal,
+    },
+    {
+      icon: RefreshIcon,
+      label: 'Request',
+      isDisabled: true,
+      onClick: () => {},
+    },
+    {
+      icon: TransactionIcon,
+      label: 'Balance',
+      onClick: onOpenBalanceModal,
+    },
+  ];
 
-        const ActionIcon = icon;
+  return (
+    <>
+      <Card className='flex flex-col gap-2 rounded-md bg-background-500 px-[6px] py-2'>
+        <Text as='h3' className='text-xs font-semibold'>
+          Quick Action
+        </Text>
+        <ul className='flex justify-between rounded-md bg-softWhite'>
+          {QUICK_ACTIONS.map((action) => {
+            const { icon, label, isDisabled, onClick } = action;
 
-        return (
-          <li key={label}>
-            <Button
-              className='flex h-full !max-h-none flex-col gap-2 !bg-transparent text-foreground-100'
-              isDisabled={isDisabled}
-              onClick={onClick}
-            >
-              <ActionIcon />
-              <Text as='span' className='text-2xs'>
-                {label}
-              </Text>
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
-  </Card>
-);
+            const ActionIcon = icon;
+
+            return (
+              <li key={label}>
+                <Button
+                  className='flex h-full !max-h-none flex-col gap-2 !bg-transparent text-foreground-100'
+                  isDisabled={isDisabled}
+                  onClick={onClick}
+                >
+                  <ActionIcon />
+                  <Text as='span' className='text-2xs'>
+                    {label}
+                  </Text>
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+      </Card>
+      {isOpenTransferModal && (
+        <TransferModal
+          isOpen={isOpenTransferModal}
+          onClose={onCloseTransferModal}
+        />
+      )}
+      {isOpenBalanceModal && (
+        <BalanceModal
+          isOpen={isOpenBalanceModal}
+          onClose={onCloseBalanceModal}
+        />
+      )}
+    </>
+  );
+};
