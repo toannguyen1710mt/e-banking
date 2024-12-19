e0a213f49a05500059ecd3505f089bf0e132239f
Ngan.Do <ngan.do@asnet.com.vn>
2024-12-19 14:44:18 +0700

    fix: allow user go back to the previous step

--- e-banking-webapp/src/components/SignUpForm/index.tsx
+++ e-banking-webapp/src/components/SignUpForm/index.tsx
@@ -114,8 +114,12 @@ export const SignUpForm = () => {
 
       <WizardForm.Footer>
         <WizardFormContextProvider>
-          {({ currentStepIndex }) => (
-            <StepProgress steps={4} activeStep={currentStepIndex} />
+          {({ currentStepIndex, prevStep }) => (
+            <StepProgress
+              prevStep={prevStep}
+              steps={4}
+              activeStep={currentStepIndex}
+            />
           )}
         </WizardFormContextProvider>
       </WizardForm.Footer>
