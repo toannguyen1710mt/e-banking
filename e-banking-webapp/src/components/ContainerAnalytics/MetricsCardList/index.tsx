a317be7082b3507e7e2519f1846b69231009c8d3
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-12-25 16:07:27 +0700

    fix: add TODO comment in MetricsCardList component

--- e-banking-webapp/src/components/ContainerAnalytics/MetricsCardList/index.tsx
+++ e-banking-webapp/src/components/ContainerAnalytics/MetricsCardList/index.tsx
@@ -2,6 +2,7 @@ import { MetricsCard } from '@/components';
 import { METRICS_CARD } from '@/mocks';
 
 export const MetricsCardList = () => (
+  // TODO: call API to get totalTransfers data
   <div className='flex flex-col gap-[10px]'>
     {METRICS_CARD.map(
       ({ title, totalTransfers, isPositive, percentageChange }, index) => (
