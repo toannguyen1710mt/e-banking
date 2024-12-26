6088120695010cfc756cb0af5fa574c81465c8c3
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-12-26 14:32:45 +0700

    refactor: update layout home

--- e-banking-webapp/src/components/ContainerHome/index.tsx
+++ e-banking-webapp/src/components/ContainerHome/index.tsx
@@ -3,11 +3,11 @@ import { Sidebar } from './Sidebar';
 
 export const ContainerHomepage = () => (
   <div className='flex h-screen w-full gap-8 px-6 pt-6'>
-    <div className='w-1/4'>
+    <div className='w-[300px]'>
       <Sidebar />
     </div>
 
-    <div className='w-3/4'>
+    <div className='grow'>
       <MainContent />
     </div>
   </div>
