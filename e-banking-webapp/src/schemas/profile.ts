57e1e2ebaf9d4b839491bd810a90d54a9ec5efd6
Ngan.Do <ngan.do@asnet.com.vn>
2024-12-27 08:43:25 +0700

    fix: remove profile schema

--- e-banking-webapp/src/schemas/profile.ts
+++ e-banking-webapp/src/schemas/profile.ts
@@ -3,31 +3,6 @@ import { z } from 'zod';
 // Constants
 import { ERROR_MESSAGES, REGEX } from '@/constants';
 
-export const ProfileSchema = z.object({
-  user: z.object({
-    username: z
-      .string()
-      .trim()
-      .min(3, ERROR_MESSAGES.USERNAME_INVALID)
-      .transform((value) => value.trim()), // Trim spaces before validation
-    email: z
-      .string()
-      .trim()
-      .email(ERROR_MESSAGES.EMAIL_INVALID)
-      .transform((value) => value.trim()), // Trim spaces before validation
-    fullName: z
-      .string()
-      .trim()
-      .min(3, ERROR_MESSAGES.USERNAME_INVALID)
-      .transform((value) => value.trim()),
-    password: z
-      .string()
-      .trim()
-      .min(8, ERROR_MESSAGES.PASSWORD_INVALID)
-      .regex(REGEX.PASSWORD, ERROR_MESSAGES.PASSWORD_PATTERN),
-  }),
-});
-
 const passwordValidation = z
   .string()
   .trim()
