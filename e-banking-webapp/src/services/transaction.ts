fb8606ca5536a8a769919cb728b50905c16e2100
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2024-12-27 08:25:25 +0700

    feat: integrate api for transfer received and transfer sent

--- e-banking-webapp/src/services/transaction.ts
+++ e-banking-webapp/src/services/transaction.ts
@@ -5,7 +5,7 @@ import { API_ENDPOINTS } from '@/constants';
 import { ITransaction, QueryParams, SuccessResponse } from '@/interfaces';
 
 // Services
-import { httpClient } from '@/services';
+import { getAccountsByUserId, httpClient } from '@/services';
 
 // Utils
 import { formatQueryParams } from '@/utils/query-params';
@@ -17,11 +17,15 @@ import { formatQueryParams } from '@/utils/query-params';
  */
 export const getTransactions = async (
   queryParams?: QueryParams,
-): Promise<ITransaction[]> => {
+): Promise<SuccessResponse<ITransaction[]>> => {
   const queryString = formatQueryParams(queryParams);
 
-  const response = await httpClient.get<SuccessResponse<ITransaction[]>>(
-    `${API_ENDPOINTS.TRANSACTIONS}${queryString ? `?${queryString}` : ''}`,
+  const endpoint = queryString
+    ? `${API_ENDPOINTS.TRANSACTIONS}?${queryString}`
+    : API_ENDPOINTS.TRANSACTIONS;
+
+  const { data } = await httpClient.get<SuccessResponse<ITransaction[]>>(
+    endpoint,
     {
       next: {
         tags: [API_ENDPOINTS.TRANSACTIONS],
@@ -29,22 +33,59 @@ export const getTransactions = async (
     },
   );
 
-  if (response?.data?.data) {
-    return response.data.data;
-  }
-
-  return [];
+  return data;
 };
 
+/**
+ * Fetches the total count of all transactions.
+ * @returns Total count of transactions.
+ */
 export const getTotalTransactions = async (): Promise<number> => {
-  const { pagination } = await httpClient.get<SuccessResponse<ITransaction[]>>(
-    API_ENDPOINTS.TRANSACTIONS,
-    {
-      next: {
-        tags: [API_ENDPOINTS.TRANSACTIONS],
+  const { meta } = await getTransactions();
+
+  return meta.pagination.total > 0 ? meta.pagination.total : 0;
+};
+
+/**
+ * Fetches the total count of transactions sent by the user.
+ * @param userId - The ID of the user.
+ * @returns Total count of sent transactions.
+ */
+export const getTotalTransferSent = async (userId: number): Promise<number> => {
+  const accounts = await getAccountsByUserId(userId);
+
+  const documentIds = accounts.map((account) => account.documentId);
+
+  const { meta } = await getTransactions({
+    filters: {
+      toAccountId: {
+        $notIn: documentIds,
       },
     },
-  );
+  });
 
-  return pagination.total > 0 ? pagination.total : 0;
+  return meta.pagination.total > 0 ? meta.pagination.total : 0;
+};
+
+/**
+ * Fetches the total count of transactions received by the user.
+ * @param userId - The ID of the user.
+ * @returns Total count of received transactions.
+ */
+export const getTotalTransferReceived = async (
+  userId: number,
+): Promise<number> => {
+  const accounts = await getAccountsByUserId(userId);
+
+  const documentIds = accounts.map((account) => account.documentId);
+
+  const { meta } = await getTransactions({
+    filters: {
+      toAccountId: {
+        $in: documentIds,
+      },
+    },
+  });
+
+  return meta.pagination.total > 0 ? meta.pagination.total : 0;
 };
