6b50f331fd20971125f71d71c16e5ff1702115fe
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-12-27 17:59:59 +0700

    feat: implement ActionCenter component

---
+++ e-banking-webapp/src/components/ActionCenter/index.tsx
@@ -0,0 +1,41 @@
+import { Card, CardBody, CardHeader } from '@nextui-org/react';
+import { Text } from '../common';
+import { MetricsCard } from '../MetricsCard';
+import { TEXT_SIZE, TEXT_VARIANT } from '@/interfaces';
+
+export const ActionCenter = () => {
+  return (
+    <Card className='gap-6 py-5 pl-5 pr-[38px]'>
+      <CardHeader className='flex-col items-start gap-6 p-0'>
+        <Text as='span'>Action Center</Text>
+        <div className='ml-[50px] flex gap-6'>
+          <MetricsCard
+            title='Transfer Received'
+            totalTransfers={54}
+            isPositive={true}
+            percentageChange={8}
+          />
+          <MetricsCard
+            title='Transfer Sent'
+            totalTransfers={14}
+            isPositive={true}
+            percentageChange={8}
+          />
+        </div>
+      </CardHeader>
+      <CardBody>
+        <Text variant={TEXT_VARIANT.DEFAULT} size={TEXT_SIZE.SM} as='span'>
+          Transfer Received
+        </Text>
+        <Text
+          as='span'
+          variant={TEXT_VARIANT.DEFAULT}
+          size={TEXT_SIZE.XS}
+          className='font-normal'
+        >
+          Manage your transfer by approving, decline request
+        </Text>
+      </CardBody>
+    </Card>
+  );
+};
--- e-banking-webapp/src/components/index.ts
+++ e-banking-webapp/src/components/index.ts
@@ -35,3 +35,4 @@ export * from './AddCreditCardModal/AddCreditCard';
 export * from './AnalyticsCard';
 export * from './DailyLimit';
 export * from './TransactionHistory';
+export * from './ActionCenter';
