33f1624ff150e74ccf45e1f42761b1fd0b71325c
Ngan.Do <ngan.do@asnet.com.vn>
2025-01-06 10:20:30 +0700

    fix: apply holdersName property

--- e-banking-webapp/src/components/AddCreditCardModal/ConfirmAddCard/index.tsx
+++ e-banking-webapp/src/components/AddCreditCardModal/ConfirmAddCard/index.tsx
@@ -61,7 +61,7 @@ export const ConfirmAddCard = ({
           <div className='flex flex-1 flex-col gap-[15px]'>
             <Controller
               control={control}
-              name='confirmationDetails.holderName'
+              name='confirmationDetails.holdersName'
               render={({ field, fieldState: { error } }) => (
                 <Input
                   labelPlacement='outside'
