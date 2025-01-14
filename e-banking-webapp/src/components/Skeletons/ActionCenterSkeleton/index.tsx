d307c41ba2772ffe5e6e43f345e8fa9397dcde8d
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-01-14 10:19:58 +0700

    fix: use Array.from() to avoid duplicating code in ActionCenterSkeleton component

--- e-banking-webapp/src/components/Skeletons/ActionCenterSkeleton/index.tsx
+++ e-banking-webapp/src/components/Skeletons/ActionCenterSkeleton/index.tsx
@@ -16,38 +16,25 @@ export const ActionCenterSkeleton = () => (
       <div className='h-5 w-2/5 rounded-lg bg-default-200' />
     </Skeleton>
     <div className='mb-[23px] ml-[50px] flex justify-center gap-6'>
-      <Card
-        className='h-[120px] w-[173px] gap-[17px] px-[13px] py-[17px]'
-        radius='sm'
-      >
-        <Skeleton className='w-full rounded-lg'>
-          <div className='h-3 w-full rounded-lg bg-default-200' />
-        </Skeleton>
-        <div className='flex justify-between gap-[15px]'>
-          <Skeleton className='w-2/5 rounded-lg'>
-            <div className='h-6 w-2/5 rounded-lg bg-default-200' />
+      {Array.from({ length: 2 }).map((_, index) => (
+        <Card
+          key={index}
+          className='h-[120px] w-[173px] gap-[17px] px-[13px] py-[17px]'
+          radius='sm'
+        >
+          <Skeleton className='w-full rounded-lg'>
+            <div className='h-3 w-full rounded-lg bg-default-200' />
           </Skeleton>
-          <Skeleton className='w-2/5 rounded-lg'>
-            <div className='h-6 w-2/5 rounded-lg bg-default-200' />
-          </Skeleton>
-        </div>
-      </Card>
-      <Card
-        className='h-[120px] w-[173px] gap-[17px] px-[13px] py-[17px]'
-        radius='sm'
-      >
-        <Skeleton className='w-full rounded-lg'>
-          <div className='h-3 w-full rounded-lg bg-default-200' />
-        </Skeleton>
-        <div className='flex justify-between gap-[15px]'>
-          <Skeleton className='w-2/5 rounded-lg'>
-            <div className='h-6 w-2/5 rounded-lg bg-default-200' />
-          </Skeleton>
-          <Skeleton className='w-2/5 rounded-lg'>
-            <div className='h-6 w-2/5 rounded-lg bg-default-200' />
-          </Skeleton>
-        </div>
-      </Card>
+          <div className='flex justify-between gap-[15px]'>
+            <Skeleton className='w-2/5 rounded-lg'>
+              <div className='h-6 w-2/5 rounded-lg bg-default-200' />
+            </Skeleton>
+            <Skeleton className='w-2/5 rounded-lg'>
+              <div className='h-6 w-2/5 rounded-lg bg-default-200' />
+            </Skeleton>
+          </div>
+        </Card>
+      ))}
     </div>
     <div className='mb-4 flex flex-col gap-2'>
       <Skeleton className='w-2/5 rounded-lg'>
