d25ad16cb312b479fb0bc8bf70a47b13e30376c3
Ngan.Do <ngan.do@asnet.com.vn>
2025-01-16 13:42:57 +0700

    feat: write unit test header component

--- e-banking-webapp/src/layouts/Header/__test__/Header.test.tsx
+++ e-banking-webapp/src/layouts/Header/__test__/Header.test.tsx
@@ -1,50 +1,135 @@
-describe('Header component', () => {
-  // const renderWithProviders = (ui: React.ReactNode) => {
-  //   return render(<ToastProvider>{ui}</ToastProvider>);
-  // };
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { redirect, usePathname } from 'next/navigation';
 
-  test('matches the snapshot', () => {
-    // const { container } = renderWithProviders(
-    //   <Header session={MOCK_SESSION_DATA} />,
-    // );
-    // expect(container).toMatchSnapshot();
+// Context
+import { ToastProvider, UserProvider } from '@/context';
+
+// Mocks
+import { MOCK_SESSION_DATA } from '@/mocks';
+
+// Actions
+import { signOut } from '@/actions';
+
+// Constants
+import { ERROR_MESSAGES } from '@/constants';
+
+// Components
+import { Header } from '..';
+
+jest.mock('@/actions', () => ({
+  signOut: jest.fn(),
+}));
+
+jest.mock('next/navigation', () => ({
+  redirect: jest.fn(),
+  usePathname: jest.fn(() => '/sign-in'),
+}));
+
+const mockShowToast = jest.fn();
+
+jest.mock('@/context', () => ({
+  ...jest.requireActual('@/context'),
+  ToastProvider: ({ children }: { children: React.ReactNode }) => {
+    return <div>{children}</div>;
+  },
+  useToastContext: () => ({
+    showToast: mockShowToast,
+  }),
+}));
+
+describe('Header component', () => {
+  const renderWithProviders = (ui: React.ReactNode) => {
+    return render(
+      <UserProvider avatar={''}>
+        <ToastProvider>{ui}</ToastProvider>
+      </UserProvider>,
+    );
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (usePathname as jest.Mock).mockReturnValue('/');
   });
 
-  test('renders the header with logo and menu items', () => {
-    // renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
-    // // Check for logo
-    // const logo = screen.getByAltText('Logo EBanking');
-    // expect(logo).toBeInTheDocument();
-    // // Check for mobile menu toggle button
-    // const menuToggle = screen.getByLabelText('Open menu');
-    // expect(menuToggle).toBeInTheDocument();
-    // // Check for avatar
-    // const avatar = screen.getByRole('img', { name: /avatar/i });
-    // expect(avatar).toBeInTheDocument();
-    // // Check for menu content
-    // const navbarItems = screen.getAllByRole('button');
-    // expect(navbarItems).toHaveLength(3); // Search, Notification, and Avatar
+  test('matches the snapshot', () => {
+    const { container } = renderWithProviders(
+      <Header session={MOCK_SESSION_DATA} />,
+    );
+    expect(container).toMatchSnapshot();
   });
 
   test('toggles the mobile menu on click', () => {
-    // renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
-    // // Find menu toggle button
-    // const menuToggle = screen.getByLabelText('Open menu');
-    // // Click to open menu
-    // fireEvent.click(menuToggle);
-    // expect(menuToggle).toHaveAttribute('aria-label', 'Close menu');
-    // // Click again to close menu
-    // fireEvent.click(menuToggle);
-    // expect(menuToggle).toHaveAttribute('aria-label', 'Open menu');
+    renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
+    // Find menu toggle button
+    const menuToggle = screen.getByLabelText('Open menu');
+    // Click to open menu
+    fireEvent.click(menuToggle);
+
+    expect(menuToggle).toHaveAttribute('aria-label', 'Close menu');
+
+    // Click again to close menu
+    fireEvent.click(menuToggle);
+    expect(menuToggle).toHaveAttribute('aria-label', 'Open menu');
   });
 
-  test('disables search and notification buttons', () => {
-    // renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
-    // const searchButton = screen.getByRole('button', { name: /search icon/i });
-    // const notificationButton = screen.getByRole('button', {
-    //   name: /notification icon/i,
-    // });
-    // expect(searchButton).toBeDisabled();
-    // expect(notificationButton).toBeDisabled();
+  test('should call signOut and redirect on successful sign out', async () => {
+    (signOut as jest.Mock).mockResolvedValueOnce(undefined);
+
+    renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
+
+    // Step 1: Open the dropdown menu by clicking the avatar
+    const avatarButton = screen.getByRole('img', {
+      name: /avatar/i,
+    });
+    fireEvent.click(avatarButton);
+
+    // Step 2: Query for the "Sign out" button inside the dropdown
+    const signOutButton = await screen.findByRole('button', {
+      name: /sign out/i,
+    });
+
+    // Step 3: Click the "Sign out" button
+    fireEvent.click(signOutButton);
+
+    // Step 4: Verify loading state and behavior
+    await waitFor(() => expect(signOutButton).toBeDisabled());
+
+    await waitFor(() => {
+      expect(signOut).toHaveBeenCalled();
+      expect(mockShowToast).toHaveBeenCalledWith(
+        ERROR_MESSAGES.SIGN_OUT_SUCCESS,
+        'success',
+        'top-center',
+      );
+      expect(redirect).toHaveBeenCalledWith('/sign-in');
+    });
+  });
+
+  test('should handle sign out failure', async () => {
+    (signOut as jest.Mock).mockRejectedValueOnce(new Error('Sign out failed'));
+
+    renderWithProviders(<Header session={MOCK_SESSION_DATA} />);
+
+    // Step 1: Open the dropdown menu by clicking the avatar
+    const avatarButton = screen.getByRole('img', {
+      name: /avatar/i,
+    });
+    fireEvent.click(avatarButton);
+
+    // Step 2: Query for the "Sign out" button inside the dropdown
+    const signOutButton = await screen.findByRole('button', {
+      name: /sign out/i,
+    });
+
+    fireEvent.click(signOutButton);
+
+    await waitFor(() => {
+      expect(signOut).toHaveBeenCalled();
+      expect(mockShowToast).toHaveBeenCalledWith(
+        ERROR_MESSAGES.SIGN_OUT_FAILED,
+        'error',
+        'top-center',
+      );
+    });
   });
 });
