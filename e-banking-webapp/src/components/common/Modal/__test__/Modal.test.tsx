ade6a13962cb7a81c73083933bc4c9a2272b1997
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2025-01-16 14:45:29 +0700

    feat: implement unit test for modal component

--- e-banking-webapp/src/components/common/Modal/__test__/Modal.test.tsx
+++ e-banking-webapp/src/components/common/Modal/__test__/Modal.test.tsx
@@ -1,33 +1,30 @@
 // Libs
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // Components
+import { Modal } from '@/components/common/Modal';
 
-// const renderModal = (
-//   isOpen: boolean,
-//   children: React.ReactNode = <div>Modal Content</div>,
-//   onClose = jest.fn(),
-// ) => {
-//   return render(
-//     <Modal isOpen={isOpen} onClose={onClose}>
-//       {children}
-//     </Modal>,
-//   );
-// };
+const renderModal = (
+  isOpen: boolean,
+  children: React.ReactNode = <div>Modal Content</div>,
+  onClose = jest.fn(),
+) => {
+  return render(
+    <Modal isOpen={isOpen} onClose={onClose}>
+      {children}
+    </Modal>,
+  );
+};
 
 describe('Modal component', () => {
   it('should match snapshot for Modal', () => {
-    // const { container } = renderModal(true);
-    // expect(container).toMatchSnapshot();
+    const { container } = renderModal(true);
+    expect(container).toMatchSnapshot();
   });
 
   it('should render the modal content when isOpen is true', () => {
-    // renderModal(true);
-    // expect(screen.getByText('Modal Content')).toBeInTheDocument();
-  });
-
-  it('should not render the modal content when isOpen is false', () => {
-    // renderModal(false);
-    // expect(screen.queryByText('Modal Content')).not.toBeInTheDocument();
+    renderModal(true);
+    expect(screen.getByText('Modal Content')).toBeInTheDocument();
   });
 });
