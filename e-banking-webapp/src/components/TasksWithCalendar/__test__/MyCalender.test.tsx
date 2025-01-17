1fd01bd75697e87a0e8f17f786df34d61eef9432
Hoang Le Minh Tran <hoang.tranle@asnet.com.vn>
2025-01-17 14:15:11 +0700

    feat: implement unit test for TasksWithCalendar

--- e-banking-webapp/src/components/TasksWithCalendar/__test__/MyCalender.test.tsx
+++
@@ -1,6 +0,0 @@
-describe('TasksWithCalendar component', () => {
-  test('should match snapshot for TasksWithCalendar', () => {
-    // const { container } = render(<TasksWithCalendar />);
-    // expect(container).toMatchSnapshot();
-  });
-});
---
+++ e-banking-webapp/src/components/TasksWithCalendar/__test__/TasksWithCalendar.test.tsx
@@ -0,0 +1,37 @@
+// Libs
+import { fireEvent, render, screen } from '@testing-library/react';
+
+// Components
+import { TasksWithCalendar } from '@/components/TasksWithCalendar';
+
+// Mock ResizeObserver to fix the ReferenceError during testing
+class MockResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+describe('TasksWithCalendar component', () => {
+  const mockDate = new Date('2025-01-01T00:00:00Z');
+
+  beforeEach(() => {
+    window.ResizeObserver = MockResizeObserver;
+
+    jest.spyOn(global.Date, 'now').mockImplementation(() => mockDate.getTime());
+  });
+
+  test('should match snapshot for TasksWithCalendar', () => {
+    const { container } = render(<TasksWithCalendar />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should update dateSelect when a date is selected in the Calendar', () => {
+    render(<TasksWithCalendar />);
+
+    const dateButton = screen.getByLabelText('Friday, January 10, 2025');
+
+    fireEvent.click(dateButton);
+
+    expect(dateButton).toHaveAttribute('data-selected', 'true');
+  });
+});
