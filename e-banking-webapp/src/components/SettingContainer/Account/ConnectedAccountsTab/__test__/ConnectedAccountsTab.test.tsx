23a43c18095cce41f9edc37a40865905f306de85
Ngan.Do <ngan.do@asnet.com.vn>
2025-01-20 16:56:44 +0700

    feat: write unit test connected tab component

--- e-banking-webapp/src/components/SettingContainer/Account/ConnectedAccountsTab/__test__/ConnectedAccountsTab.test.tsx
+++ e-banking-webapp/src/components/SettingContainer/Account/ConnectedAccountsTab/__test__/ConnectedAccountsTab.test.tsx
@@ -1,6 +1,21 @@
+import { render, screen } from '@testing-library/react';
+
+// Components
+import { ConnectedAccountsTab, PAYMENT_METHODS } from '..';
+
 describe('ConnectedAccountsTab component', () => {
   it('should match snapshot for ConnectedAccountsTab', () => {
-    // const { container } = render(<ConnectedAccount />);
-    // expect(container).toMatchSnapshot();
+    const { container } = render(<ConnectedAccountsTab />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render buttons for each payment method with correct label and button text', () => {
+    render(<ConnectedAccountsTab />);
+
+    PAYMENT_METHODS.forEach(({ label, buttonText }) => {
+      expect(screen.getByText(label)).toBeInTheDocument();
+      expect(screen.getByText(buttonText)).toBeInTheDocument();
+    });
   });
 });
