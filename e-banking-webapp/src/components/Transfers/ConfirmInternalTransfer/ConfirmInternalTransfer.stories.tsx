2334c1d04855657fac785c82a87b01561671386a
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-01-24 10:09:24 +0700

    fix: fix error cannot render storybook in ConfirmInternalTransfer component

--- e-banking-webapp/src/components/Transfers/ConfirmInternalTransfer/ConfirmInternalTransfer.stories.tsx
+++ e-banking-webapp/src/components/Transfers/ConfirmInternalTransfer/ConfirmInternalTransfer.stories.tsx
@@ -1,8 +1,16 @@
 // Libs
 import type { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+
+// Interfaces
+import { AccountType } from '@/interfaces';
+
+// Utils
+import { InternalTransferFormSchema } from '@/schemas';
 
 // Components
 import { ConfirmInternalTransfer } from '@/components';
+import * as WizardForm from '@/components/common/WizardForm';
 
 const meta = {
   title: 'Components/Transfers/ConfirmInternalTransfer',
@@ -17,6 +25,40 @@ export default meta;
 
 type Story = StoryObj<typeof ConfirmInternalTransfer>;
 
+const useMockForm = () => {
+  return useForm({
+    defaultValues: {
+      fromAccountId: '',
+      fromCardName: '',
+      fromAccountNumber: '',
+      fromAccountBalance: 0,
+      toAccountId: '',
+      internalTransfer: {
+        amount: '500',
+        fromAccountType: '' as AccountType,
+        toAccountType: '' as AccountType,
+      },
+      toCardName: '',
+      toAccountNumber: '',
+      toAccountBalance: 0,
+    },
+  });
+};
+
 export const Default: Story = {
-  args: {},
+  render: (args) => (
+    <WizardForm.Root
+      schema={InternalTransferFormSchema}
+      form={useMockForm()}
+      className='flex grow flex-col'
+    >
+      <WizardForm.Step
+        name='confirm'
+        key='confirm'
+        className='flex grow flex-col items-center justify-center'
+      >
+        <ConfirmInternalTransfer {...args} />
+      </WizardForm.Step>
+    </WizardForm.Root>
+  ),
 };
