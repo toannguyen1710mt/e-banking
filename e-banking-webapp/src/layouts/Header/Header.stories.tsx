ffe1bdb1946dd80abc14b03d8352ba19d6d8a022
Ngan.Do <ngan.do@asnet.com.vn>
2025-01-24 10:08:44 +0700

    fix: update properties correctly

--- e-banking-webapp/src/layouts/Header/Header.stories.tsx
+++ e-banking-webapp/src/layouts/Header/Header.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 // Layouts
 import { Header } from '@/layouts';
-import { MOCK_SESSION_DATA } from '@/mocks';
 
 const meta = {
   title: 'Layouts/Header',
@@ -19,6 +18,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    session: MOCK_SESSION_DATA,
+    username: 'John Doe',
+    email: 'test@example.com',
   },
 };
