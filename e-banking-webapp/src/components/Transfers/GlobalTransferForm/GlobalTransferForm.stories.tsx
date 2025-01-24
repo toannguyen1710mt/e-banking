2e49d17d605e7791ccddd0c39bdf55265746676d
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-01-24 10:17:02 +0700

    fix: fix error cannot render storybook in GlobalTransferForm component

--- e-banking-webapp/src/components/Transfers/GlobalTransferForm/GlobalTransferForm.stories.tsx
+++ e-banking-webapp/src/components/Transfers/GlobalTransferForm/GlobalTransferForm.stories.tsx
@@ -1,11 +1,18 @@
 // Libs
 import { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
+
+// Constants
+import { MOCK_SESSION_DATA } from '@/mocks';
+
+// Models
+import { AccountType, GlobalType } from '@/interfaces';
+import { GlobalTransferFormSchema } from '@/schemas';
 
 // Components
+import * as WizardForm from '@/components/common/WizardForm';
 import { GlobalTransferForm } from '@/components';
-
-// Mocks
-import { MOCK_SESSION_DATA } from '@/mocks';
+import { FetchedBalancesProvider } from '@/context';
 
 const meta = {
   title: 'Components/Transfers/GlobalTransferForm',
@@ -27,8 +34,36 @@ export default meta;
 
 type Story = StoryObj<typeof GlobalTransferForm>;
 
+const useMockForm = () => {
+  return useForm({
+    defaultValues: {
+      globalTransfer: {
+        fromAccountType: '' as AccountType,
+        fromCountryType: '' as GlobalType,
+        recipientAccount: '',
+        amount: '',
+      },
+      fromAccountId: '',
+      fromCardName: '',
+      fromAccountNumber: '',
+      fromAccountBalance: 0,
+      recipientName: '',
+    },
+  });
+};
+
 export const InternalTransferFormDefault: Story = {
-  args: {
-    session: MOCK_SESSION_DATA,
-  },
+  render: () => (
+    <FetchedBalancesProvider>
+      <WizardForm.Root
+        schema={GlobalTransferFormSchema}
+        form={useMockForm()}
+        className='flex grow flex-col'
+      >
+        <WizardForm.Step name='transfer' key='transfer'>
+          <GlobalTransferForm session={MOCK_SESSION_DATA} />
+        </WizardForm.Step>
+      </WizardForm.Root>
+    </FetchedBalancesProvider>
+  ),
 };
