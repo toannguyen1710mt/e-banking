8bc62ba480a67c20433a4596d9c686578b6b9605
Ngan.Do <ngan.do@asnet.com.vn>
2025-02-14 11:07:07 +0700

    fix: update attributes name to camelCase conventions

--- e-banking-webapp/src/components/icons/AttentionIcon.tsx
+++ e-banking-webapp/src/components/icons/AttentionIcon.tsx
@@ -6,27 +6,27 @@ export const AttentionIcon = () => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <g clip-path='url(#clip0_1_18315)'>
+    <g clipPath='url(#clip0_1_18315)'>
       <path
         d='M4.58484 1.16675H9.41484L12.8332 4.58508V9.41508L9.41484 12.8334H4.58484L1.1665 9.41508V4.58508L4.58484 1.16675Z'
         stroke='#808080'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M7 9.33325H7.00583'
         stroke='#808080'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M7 4.66675V7.00008'
         stroke='#808080'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </g>
     <defs>
